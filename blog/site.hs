093373c9e0a03bcd80d2b609eb72ac6219665a0f
Jose Storopoli <jose@storopoli.com>
2025-06-09 19:45:48 -0300

    feat: tags

--- blog/site.hs
+++ blog/site.hs
@@ -65,14 +65,31 @@ main = hakyllWith configuration $ do
           >>= loadAndApplyTemplate "templates/default.html" defaultContext
           >>= relativizeUrls
 
+  -- build up tags
+  tags <- buildTags "posts/*.md" (fromCapture "tags/*.html")
+  tagsRules tags $ \tag pattern -> do
+    let title = "Posts tagged \"" ++ tag ++ "\""
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
+      let ctx =
+            constField "title" title
+              `mappend` listField "posts" (postCtxWithTags tags) (return posts)
+              `mappend` defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tags.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
   -- Posts markdown
   match "posts/*.md" $ do
     route $ setExtension "html"
     compile $
       pandocCompiler'
-        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/post.html" (postCtxWithTags tags)
         >>= saveSnapshot "content"
-        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
         >>= relativizeUrls
 
   -- Archive
@@ -127,6 +144,9 @@ postCtx :: Context String
 postCtx =
   dateField "date" "%B %e, %Y"
     `mappend` defaultContext
+
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
 
 --------------------------------------------------------------------------------
 -- Configuration
